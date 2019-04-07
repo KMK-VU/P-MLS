1ed077f6173af2d3dfabc04798b0f82675162dab
Kevin Vu <kevin.mkh.vu@gmail.com>
2019-04-07 15:32:11 +0200

    Fix error + add html format

--- js-browser/pdas-data.js
+++ js-browser/pdas-data.js
@@ -12,8 +12,7 @@ class dataframe {
 	/////////////////////////////////////////////////////////////////
 	// read_csv : Function used to read big csv file  (streaming) //
 	///////////////////////////////////////////////////////////////
-	read_csv(arg){
-
+	read_csv(arg, callback=this._empty_func, args=[]){
 		// Parsing papaparse data to the core data structures
 		var result = {};
 		var to_json = function to_json(papaparse){
@@ -35,18 +34,17 @@ class dataframe {
 		this.isDataReady = false;
 		var self = this;
 		Papa.parse(arg, { header: true, download:true, dynamicTyping: true,
-      delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP]
+			complete: function(results) {
+			self._update_data(to_json(results.data));
+			callback(...args);
+			},
+      delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP]})
 }
-
-
-		complete:function(results){self._update_data(to_json(results.data));}});
-
-	}
 
 	///////////////////////////////////////////////////////////////////////////////////
 	// read_excel : Function used to read excel file small to medium excel/csv file //
 	/////////////////////////////////////////////////////////////////////////////////
-	read_excel(arg){
+	read_excel(arg, callback=this._empty_func, args=[]){
 		this.isDataReady = false;
 		// Parsing workbook result to the core data structure
 		var to_json = function to_json(workbook) {
@@ -63,9 +61,28 @@ class dataframe {
 		var data = e.target.result;
 		var converted = XLSX.read(data, {type : 'binary'});
 		self._update_data(to_json(converted));
+		callback(...args);
 		};
 		reader.readAsBinaryString(arg);
+
 	}
+
+
+  flt(){
+
+  }
+
+  getvar(){
+
+  }
+
+  getvars(){
+
+  }
+
+  getlines(){
+
+  }
 
 	//////////////////////////////////////////////////////////////////////////////////////////
 	// _update_data : Function used to update data structure when parsing task is finished //
@@ -96,6 +113,65 @@ class dataframe {
 		console.log("data: ",this.json_data);
 		console.log("variables: ",this.columns);
 		console.log("shapes: ",this.shape);
+	}
+
+	///////////////////////////////////////
+	// Optional : show results in html  //
+	/////////////////////////////////////
+
+	sheet_html(sheetshow_id){
+		var result = "";
+		var sheetnames = Object.keys(this.columns);
+		sheetnames.forEach(function(element){
+			result += `<option value="${element}">${element}</option>`
+		});
+		var sheetshow = document.getElementById(sheetshow_id);
+		sheetshow.innerHTML = result;
+
+	}
+	variable_html(sheetname, var_id){
+		var result =  "";
+		var columns = this.columns[sheetname];
+		columns.forEach(function(element){
+			result += `<option value="${element}">${element}</option>`
+		});
+		var varselect = document.getElementById(var_id);
+		varselect.innerHTML = result;
+	}
+
+	table_html(sheetname, table_id){
+		// Write header.
+		var result ="<thead><tr>";
+		this.columns[sheetname].forEach(function(element) {
+  		result +=`<th>${element}</th>`
+		});
+		result +="</tr></thead>";
+		// Write core column.
+		result +="<tbody>";
+		this.json_data[sheetname].forEach(function(element){
+			result +="<tr>";
+			element.forEach(function(element){
+				result +=`<td>${element}</td>`
+			});
+			result +="</tr>";
+		});
+
+		result += "</tbody>";
+
+		var table = document.getElementById(table_id);
+		table.innerHTML = result;
+
+	}
+
+	select_sheet(sheet_id, variable_id, table_id){
+		var sheetname = document.getElementById(sheet_id);
+		this.variable_html(sheetname.value, variable_id);
+		this.table_html(sheetname.value, table_id);
+	}
+
+
+	_empty_func(){
+
 	}
 
 }
